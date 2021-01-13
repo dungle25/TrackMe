2d4db969d59da2b6fe831489c22129c8285c5027
Dung Le <dung.le@deliveree.com>
2021-01-13 22:35:53 +0700

    Try to fix bug cannot insert to database when comeback from background due to viewmodelscope.launch exception

--- app/src/main/java/com/dungle/getlocationsample/model/Session.kt
+++ app/src/main/java/com/dungle/getlocationsample/model/Session.kt
@@ -2,17 +2,18 @@ package com.dungle.getlocationsample.model
 
 import android.location.Location
 import android.os.Parcelable
+import androidx.room.Relation
 import com.dungle.getlocationsample.data.session.local.model.LocalSession
+import com.google.gson.Gson
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Session(
-    var id: Int = 0,
-    var locations: MutableList<Location> = arrayListOf(),
+    @Relation(parentColumn = "locations", entityColumn = "locations")  var locations: MutableList<Location> = arrayListOf(),
     var displayAvgSpeed: Double = 0.0,
-    var speeds: MutableList<Double> = arrayListOf(),
-    var distance: Double = 0.0,
-    var displayDuration: String = "00:00:00",
+    @Relation(parentColumn = "speeds", entityColumn = "speeds")  var speeds: MutableList<Double> = arrayListOf(),
+    @Relation(parentColumn = "distance", entityColumn = "distance") var distance: Double = 0.0,
+    @Relation(parentColumn = "displayDuration", entityColumn = "displayDuration")  var displayDuration: String = "00:00:00",
     var duration: Long = 0L,
     var startTime: Long = 0L,
     var endTime: Long = 0L,
