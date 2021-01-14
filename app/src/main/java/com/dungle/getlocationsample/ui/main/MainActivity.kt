45980151b9a765afd5cb9f7f64bb2fc6c152d6d0
Dung Le <dung.le@deliveree.com>
2021-01-14 20:21:55 +0700

    Fix crash

--- app/src/main/java/com/dungle/getlocationsample/ui/main/MainActivity.kt
+++ app/src/main/java/com/dungle/getlocationsample/ui/main/MainActivity.kt
@@ -4,10 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import com.dungle.getlocationsample.Constant
 import com.dungle.getlocationsample.R
-import com.dungle.getlocationsample.TrackingStatus
 import com.dungle.getlocationsample.ui.history.HistoryFragmentDirections
 import com.dungle.getlocationsample.util.LocationUpdateUtils
 
@@ -29,7 +27,9 @@ class MainActivity : AppCompatActivity() {
         val extras = intent.extras
         if (extras != null) {
             val fromNotification = extras.getBoolean(Constant.FROM_NOTIFICATION, false)
-            if (fromNotification && LocationUpdateUtils.getCurrentTrackingStatus(this) != TrackingStatus.STOPPED ) {
+            val isRunning = LocationUpdateUtils.isTrackingServiceRunning(this)
+            if (fromNotification && isRunning) {
+                intent.removeExtra(Constant.FROM_NOTIFICATION)
                 findNavController(R.id.nav_host_fragment).navigate(HistoryFragmentDirections.actionHistoryFragmentToRecordFragment())
             }
         }
